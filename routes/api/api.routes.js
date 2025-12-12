b959c109cfc624152a1cecb2ef865d33677ad01c
nithipat22 <wannee857@gmail.com>
2025-12-12 16:08:51 +0700

    x

--- routes/api/api.routes.js
+++ routes/api/api.routes.js
@@ -42,6 +42,7 @@ router.get('/example', (req, res) => {
     }
     ])
 })
+// asdasdasdasd
 // auth
 router.post('/register', require('../../controller/auth/register.controller'))
 router.post('/login', require('../../controller/auth/login.controller'))
