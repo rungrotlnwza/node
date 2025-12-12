ea480a0065340ead3a1abe62e32ecac330111656
Rungrot <rungrot.muangchalerm@gmail.com>
2025-12-12 16:05:44 +0700

    example

---
+++ controller/public/example.controller.js
@@ -0,0 +1,41 @@
+module.exports = async function (req, res) {
+    res.status(200).json([{
+        "title": "เทคนิคการเรียนภาษาอังกฤษ",
+        "description": "อัพเดทบทความใหม่! 10 เคล็ดลับ...",
+        "date": "2025-12-06",
+        "link": "https://example.com/article/1",
+        "background": "./assets/img/earth.webp",
+        "images": [
+            "./assets/img/earth.webp",
+            "./assets/img/earth.webp",
+            "./assets/img/earth.webp"
+        ]
+    },
+    {
+        "title": "วิธีเพิ่มความเร็วการอ่านภาษาอังกฤษ",
+        "description": "เทคนิคสุดง่ายที่ใครก็ทำได้...",
+        "date": "2025-12-06",
+        "link": "https://example.com/article/2",
+        "background": "./assets/img/cards.webp",
+        "images": [
+            "./assets/img/earth.webp",
+            "./assets/img/earth.webp",
+            "./assets/img/earth.webp",
+            "./assets/img/earth.webp"
+        ]
+    },
+    {
+        "title": "วิธีเพิ่มความเร็วการอ่านภาษาอังกฤษ",
+        "description": "เทคนิคสุดง่ายที่ใครก็ทำได้...",
+        "date": "2025-12-06",
+        "link": "https://example.com/article/2",
+        "background": "./assets/img/cards.webp",
+        "images": [
+            "./assets/img/earth.webp",
+            "./assets/img/earth.webp",
+            "./assets/img/earth.webp",
+            "./assets/img/earth.webp"
+        ]
+    }
+    ])
+}
