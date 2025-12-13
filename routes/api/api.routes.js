f1c3aa36fe4d88b4722395a0b359b28a8e9cbf41
Rungrot <rungrot.muangchalerm@gmail.com>
2025-12-13 18:46:54 +0700

    แก้ router ของ example ให้ไช้ controller

--- routes/api/api.routes.js
+++ routes/api/api.routes.js
@@ -1,47 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/example', (req, res) => {
-    res.status(200).json([{
-        "title": "เทคนิคการเรียนภาษาอังกฤษ",
-        "description": "อัพเดทบทความใหม่! 10 เคล็ดลับ...",
-        "date": "2025-12-06",
-        "link": "https://example.com/article/1",
-        "background": "./assets/img/earth.webp",
-        "images": [
-            "./assets/img/earth.webp",
-            "./assets/img/earth.webp",
-            "./assets/img/earth.webp"
-        ]
-    },
-    {
-        "title": "วิธีเพิ่มความเร็วการอ่านภาษาอังกฤษ",
-        "description": "เทคนิคสุดง่ายที่ใครก็ทำได้...",
-        "date": "2025-12-06",
-        "link": "https://example.com/article/2",
-        "background": "./assets/img/cards.webp",
-        "images": [
-            "./assets/img/earth.webp",
-            "./assets/img/earth.webp",
-            "./assets/img/earth.webp",
-            "./assets/img/earth.webp"
-        ]
-    },
-    {
-        "title": "วิธีเพิ่มความเร็วการอ่านภาษาอังกฤษ",
-        "description": "เทคนิคสุดง่ายที่ใครก็ทำได้...",
-        "date": "2025-12-06",
-        "link": "https://example.com/article/2",
-        "background": "./assets/img/cards.webp",
-        "images": [
-            "./assets/img/earth.webp",
-            "./assets/img/earth.webp",
-            "./assets/img/earth.webp",
-            "./assets/img/earth.webp"
-        ]
-    }
-    ])
-})
+router.get('/example', require('../../controller/public/example.controller'))
 // auth
 router.post('/register', require('../../controller/auth/register.controller'))
 router.post('/login', require('../../controller/auth/login.controller'))
