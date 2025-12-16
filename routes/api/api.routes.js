6c793b3f152c5e522850741e9d98f788d99c6938
Rungrot <rungrot.muangchalerm@gmail.com>
2025-12-16 13:17:59 +0700

    api mockup lesson

--- routes/api/api.routes.js
+++ routes/api/api.routes.js
@@ -8,4 +8,7 @@ router.post('/register', require('../../lib/controller/auth/register.controller'
 router.post('/login', require('../../lib/controller/auth/login.controller'))
 router.get('/me', verifyToken, require('../../lib/controller/auth/me.controller'))
 router.put('/update', verifyToken, require('../../lib/controller/auth/updateme.controller'))
+
+// lesson
+router.get('/lesson', verifyToken, require('../../lib/controller/lesson/lesson.controller'))
 module.exports = router
