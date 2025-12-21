4b03dbe9ed95a10bf5724d04f32b0c4337f464a1
Rungrot <rungrot.muangchalerm@gmail.com>
2025-12-21 16:51:26 +0700

    อัพเดท bug 2

--- routes/api/api.routes.js
+++ routes/api/api.routes.js
@@ -11,16 +11,7 @@ router.post('/login', require('../../lib/controller/auth/login.controller'))
 router.get('/me', verifyToken, require('../../lib/controller/auth/me.controller'))
 router.put('/update', verifyToken, require('../../lib/controller/auth/updateme.controller'))
 
-// 1. ดึงข้อมูลทั้งหมดแบบ Pagination (ไม่มี parameter)
-router.get('/lessonAll', verifyToken, require('../../lib/controller/lesson/getAllLessons'))
-// 2. ดึงข้อมูลทั้งหมดของหมวดหมู่ (1 parameter พร้อม prefix)
-router.get('/lesson/category/:category', verifyToken, require('../../lib/controller/lesson/getLessonsByCategory'))
-// 3. ดึงแบบกรองคู่ หมวดหมู่ + ระดับ (2 parameters)
-router.get('/lesson/filter/:category/:level', verifyToken, require('../../lib/controller/lesson/getLessonsByFilter'))
-// 4. ดึงข้อมูลแบบ Step สำหรับเรียน (เจาะจงที่สุด: 3 parameters)
-router.get('/lesson/detail/:category/:level/:step', verifyToken, require('../../lib/controller/lesson/getLessonByStep'))
-// 5. ดึงด้วย ID (1 parameter - ต้องวางล่างสุดเพื่อไม่ให้ตีกับ /all หรือ /category)
-router.get('/lesson/:id', verifyToken, require('../../lib/controller/lesson/getLessonById'))
+
 
 // --- อื่นๆ ---
 router.post('/add-lesson', verifyToken, upload, require('../../lib/controller/lesson/add-lesson.controller'))
