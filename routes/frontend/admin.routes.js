4ef806e0d6373983df941fda191b5b78a17f2004
Rungrot <rungrot.muangchalerm@gmail.com>
2025-12-21 16:44:30 +0700

    อัพเดท bug แน่นอน

--- routes/frontend/admin.routes.js
+++ routes/frontend/admin.routes.js
@@ -3,15 +3,19 @@ const router = express.Router()
 const path = require('path');
 // admin
 router.get('/private/admin', (req, res) => [
-    res.render(path.join(__dirname, '../../views/page/admin/admin.ejs'), {
+    res.render(path.join(__dirname, '../../views/page/private/admin/admin.ejs'), {
         layout: path.join(__dirname, '../../views/layouts/admin.layout.ejs')
     })
 ])
-router.get('/admin/addlesson', (req, res) => {
-    res.render(path.join(__dirname, '../../views/page/admin/add-lesson.ejs'), {
+router.get('/private/addlesson', (req, res) => {
+    res.render(path.join(__dirname, '../../views/page/private/admin/add-lesson.ejs'), {
         layout: path.join(__dirname, '../../views/layouts/admin.layout.ejs')
     })
 })
-
+router.get('/private/update-lesson', (req, res) => {
+    res.render(path.join(__dirname, '../../views/page/private/admin/update-lesson.ejs'), {
+        layout: path.join(__dirname, '../../views/layouts/admin.layout.ejs')
+    })
+})
 
 module.exports = router
